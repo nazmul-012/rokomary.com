bfe11ffbec854e36269230b8193748cf348d6193
Nazmul Hossain <nahidnazmul000@gmail.com>
2021-03-25 15:14:09 +0600

    js function

--- assets/js/control.js
+++ assets/js/control.js
@@ -7,3 +7,28 @@ document.getElementById('logIn').addEventListener('click', function(){
     document.getElementById('logIn').className="new-active";
     document.getElementById('signUp').className="new-stop";
 });
+
+
+document.getElementById('btm-line1').addEventListener('click', function(){
+   document.getElementById('div-border').className="div-border";
+});
+document.getElementById('btm-line2').addEventListener('click', function(){
+    document.getElementById('div-border2').className="div-border";
+ });
+
+ document.getElementById('btm-line1').addEventListener('keyup', function(){
+    document.getElementById('div-border').className="div-border-edit";
+ });
+ document.getElementById('btm-line2').addEventListener('keyup', function(){
+     document.getElementById('div-border2').className="div-border-edit";
+  });
+       function blurColor() {
+           var emailName = document.getElementById('btm-line1').nodeValue;
+       }
+ document.getElementById('btm-line2').addEventListener('blur', function(){
+    var emailName = document.getElementById('btm-line2').nodeValue;
+    alert(emailName);
+     document.getElementById('div-border2').className="div-border-edit-blur";
+  });
+  
+ 
